a0c8bca3e4917c858f8dd2d9712d571b5ae35aca
Tamas K. Papp <tkpapp@gmail.com>
2019-05-29 16:03:33 +0200

    fix path

--- install_stan.sh
+++ install_stan.sh
@@ -2,8 +2,8 @@
 echo "******************** working directory is"
 echo `pwd`
 echo "******************** adding packages"
-sudo apt-get update
-sudo apt-get install wget tar make
+# sudo apt-get update
+# sudo apt-get install wget tar make
 echo "******************** downloading and extracting Stan"
 wget https://github.com/stan-dev/cmdstan/releases/download/v2.19.1/cmdstan-2.19.1.tar.gz
 tar -xzpf cmdstan-2.19.1.tar.gz
